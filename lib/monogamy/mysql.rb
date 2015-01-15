3df8220d946a8709c42f0c134835f038fc6d9daa
David Perkowski <david.perkowski@soomolearning.com>
2015-01-15 13:56:50 -0500

    Adjust ordering and introduce autocommit handling
    This appears to more closely follow the guidelines on the cited MySQL manual page.

--- lib/monogamy/mysql.rb
+++ lib/monogamy/mysql.rb
@@ -2,13 +2,16 @@ module Monogamy
   module MySQL
     # See http://dev.mysql.com/doc/refman/5.0/en/lock-tables-and-transactions.html
     def self.with_table_lock(connection, quoted_table_name, &block)
+      auto_commit = connection.execute("SELECT @@AUTOCOMMIT FROM DUAL").to_a.flatten.first
       begin
+        connection.execute("SET autocommit=0")
+        connection.execute("LOCK TABLES #{quoted_table_name} WRITE")
         connection.transaction do
-          connection.execute("LOCK TABLES #{quoted_table_name} WRITE")
           yield
         end
       ensure
         connection.execute("UNLOCK TABLES")
+        connection.execute("SET autocommit=#{auto_commit}")
       end
     end
   end
